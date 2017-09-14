8282de953cea8bf2aeacbdf585f1a9bec40170aa
Arpit Anand <mjarpitanand@gmail.com>
2017-09-14 16:32:48 +0530

    Company Profile page

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,13 +11,40 @@ function toggle(){
     });
 
 }
-
-
 $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-
 });
+
+$('#modal1').modal('open');
+
 $(document).ready(function() {
     $('select').material_select();
 });
+setInterval(function(){
+    function doNotification(){
+        var myNotification = new Notify('Yo dawg!', {
+            body: 'This is an awesome notification',
+            icon : '../img/m_logo.png',
+            notifyShow: onNotifyShow
+        });
+        function onNotifyShow() {
+            console.log('notification was shown!');
+        } 
+        myNotification.show(); 
+    }
+
+    if (!Notify.needsPermission) {
+        doNotification();
+    } else if (Notify.isSupported()) {
+        Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+    }
+
+    function onPermissionGranted() {
+        console.log('Permission has been granted by the user');
+        doNotification();
+    }
+
+    function onPermissionDenied() {
+        console.warn('Permission has been denied by the user');
+    }
+} , 500000);
